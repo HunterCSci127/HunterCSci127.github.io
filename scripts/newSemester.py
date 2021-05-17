2be4918fd3ec1a62db3c0326020013291cb40e41
Owen Kunhardt <okunhardt@users.noreply.github.com>
2021-05-17 12:22:14 -0400

    Updated new semester script

--- scripts/newSemester.py
+++ scripts/newSemester.py
@@ -25,13 +25,13 @@ if words[0] not in ["Fall", "Spring", "Summer"] or len(words[1]) != 4:
 
 
 # Changes semester names to match file path format
-if oldSemesterName.find("Summer") > -1:
+if "Summer" in oldSemesterName:
     oldName = "summer" + oldSemesterName[-2] + oldSemesterName[-1]
 
 else:
     oldName = oldSemesterName[0].lower() + oldSemesterName[-2] + oldSemesterName[-1]
 
-if newSemesterName.find("Summer") > -1:
+if "Summer" in newSemesterName:
     newName = "summer" + newSemesterName[-2] + newSemesterName[-1]
 
 else:
@@ -67,41 +67,40 @@ replacer.replace("../" + newName + "/honors.html", oldSemesterName, newSemesterN
 replacer.replace("../" + newName + "/final/final.html", oldSemesterName, newSemesterName)
 
 # Changes month assignment is due based in on whether new semester is fall or spring or summer
-if newName[0] == 'f':
+if "Spring" in oldSemesterName and "Fall" in newSemesterName:
     replacer.replace("../" + newName + "/ps.html", "February", "September")
     replacer.replace("../" + newName + "/ps.html", "March", "October")
     replacer.replace("../" + newName + "/ps.html", "April", "November")
     replacer.replace("../" + newName + "/ps.html", "May", "December")
 
-elif newName.find("summer") > -1:
+elif "Spring" in oldSemesterName and "Summer" in newSemesterName:
     replacer.replace("../" + newName + "/ps.html", "February", "June")
     replacer.replace("../" + newName + "/ps.html", "March", "June")
     replacer.replace("../" + newName + "/ps.html", "April", "June")
     replacer.replace("../" + newName + "/ps.html", "May", "July")
 
-else:
+elif "Fall" in oldSemesterName and "Spring" in newSemesterName:
     replacer.replace("../" + newName + "/ps.html", "September", "February")
     replacer.replace("../" + newName + "/ps.html", "October", "March")
     replacer.replace("../" + newName + "/ps.html", "November", "April")
     replacer.replace("../" + newName + "/ps.html", "December", "May")
 
 # Changes month lecture occurs based on whether new semester is fall or spring or summer
-if newName[0] == 'f':
+if "Spring" in oldSemesterName and "Fall" in newSemesterName:
     replacer.replace(newHtml, "January", "August")
     replacer.replace(newHtml, "February", "September")
     replacer.replace(newHtml, "March", "October")
     replacer.replace(newHtml, "April", "November")
     replacer.replace(newHtml, "May", "December")
 
-elif newName.find("summer") > -1:
-
+elif "Spring" in oldSemesterName and "Summer" in newSemesterName:
     replacer.replace(newHtml, "January", "May")
     replacer.replace(newHtml, "February", "June")
     replacer.replace(newHtml, "March", "June")
     replacer.replace(newHtml, "April", "June")
     replacer.replace(newHtml, "May", "July")
 
-else:
+elif "Fall" in oldSemesterName and "Spring" in newSemesterName:
     replacer.replace(newHtml, "August", "January")
     replacer.replace(newHtml, "September", "February")
     replacer.replace(newHtml, "October", "March")
@@ -110,20 +109,19 @@ else:
 
 
 # Changes month quiz occurs based in on whether new semester is fall or spring or summer
-if newName[0] == 'f':
+if "Spring" in oldSemesterName and "Fall" in newSemesterName:
     replacer.replace("../" + newName + "/quizzes.html", "February", "September")
     replacer.replace("../" + newName + "/quizzes.html", "March", "October")
     replacer.replace("../" + newName + "/quizzes.html", "April", "November")
     replacer.replace("../" + newName + "/quizzes.html", "May", "December")
 
-elif newName.find("summer") > -1:
-
+elif "Spring" in oldSemesterName and "Summer" in newSemesterName:
     replacer.replace("../" + newName + "/quizzes.html", "February", "June")
     replacer.replace("../" + newName + "/quizzes.html", "March", "June")
     replacer.replace("../" + newName + "/quizzes.html", "April", "June")
     replacer.replace("../" + newName + "/quizzes.html", "May", "July")
 
-else:
+elif "Fall" in oldSemesterName and "Spring" in newSemesterName:
     replacer.replace("../" + newName + "/quizzes.html", "September", "February")
     replacer.replace("../" + newName + "/quizzes.html", "October", "March")
     replacer.replace("../" + newName + "/quizzes.html", "November", "April")
