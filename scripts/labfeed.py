d446ca4db1a1f61bb293597f17b5e6c31d5f454c
yumandee <yumandee@yahoo.com>
2021-07-29 21:30:07 -0400

    Update labfeed.py

--- scripts/labfeed.py
+++ scripts/labfeed.py
@@ -1,22 +1,23 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
 import pandas as pd
-  
-# Initializing a GoogleAuth Object
-gauth = GoogleAuth()
-  
-# client_secrets.json file is verified
-# and it automatically handles authentication
-gauth.LocalWebserverAuth()
-  
-# GoogleDrive Instance is created using
-# authenticated GoogleAuth instance
-drive = GoogleDrive(gauth)
-  
-# Initialize GoogleDriveFile instance with file id
-file_obj = drive.CreateFile({'id': 'FILE_ID'})
-file_obj.GetContentFile('FILE_NAME.xls',
-         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-  
-dataframe = pd.read_excel('FILE_NAME.xls')
-print(dataframe)
+import json
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+SCOPE = ["https://spreadsheets.google.com/feeds"]
+SECRETS_FILE = "client_secrets.json"
+SPREADSHEET = "CSci 127 Tutoring Sign-In (Responses)"
+# Based on docs here - http://gspread.readthedocs.org/en/latest/oauth2.html
+# Load in the secret JSON key (must be a service account)
+json_key = json.load(open(SECRETS_FILE))
+# Authenticate using the signed key
+credentials = SignedJwtAssertionCredentials(json_key['client_email'],
+                                            json_key['private_key'], SCOPE)
+gc = gspread.authorize(credentials)
+# Open up the workbook based on the spreadsheet name
+workbook = gc.open(SPREADSHEET)
+# Get the first sheet
+sheet = workbook.sheet1
+# Extract all data into a dataframe
+results = pd.DataFrame(sheet.get_all_records())
