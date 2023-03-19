7b9cb3adec2f5041a06f29b2c6fb7cbe8c43b35e
LaptopUser <tsmile2006@yahoo.com>
2023-03-18 20:30:56 -0400

    p45 of s23

---
+++ files/errorNames.py
@@ -0,0 +1,44 @@
+# Name: 
+# Email:
+# Program #45: Correct the errors in the program to output the correct results.
+
+""" printNames(nameList) prints out ONCE Five most popular female names in the USA, then print every name on the list of nameList, one per line """
+def printNames(nameList):
+    for name in nameList:
+        print("Five most popular female names in USA: ")
+        print(name 
+    Print()
+  
+""" inList(nameList, nameToSearch) checks if name is a name on the nameList and prints out a message accordingly """     
+def inList(nameList, nameToSearch):    
+    found == False
+    for name in nameList:
+        if name = nameToSearch
+            found == True
+            
+    if not found:
+    print(nameToSearch + " is in the list of names.")
+    else:
+        print(nameToSearch " is not in the list of names.")
+        
+""" numNames(nameList) return a number of names in the list"""                
+def numNames(nameList):
+num = length(namelist)
+    return "There are " + str[n) " names in the list."
+
+def main():
+    # A list of five most popular female names in the USA 
+    nameList = ["Olivia", "Emma" "Charlotte", "Amelia", "Ava"
+    
+    printNames(nameList)
+
+    inList(nameList, "Olivia")
+    isList(namelist, 'Emma")          
+    isList(nameList, "Mary")
+    itList(nameList, "Helen')
+    
+    print(numName(nameList))
+
+    
+if __name__ == '__main()__":
+    main()
